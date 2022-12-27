e18b03ed9ce5e280ea3926b8c7c3ca6031bdbf51
h-twins <creative.h.twins@gmail.com>
2022-12-27 15:06:27 -0500

    database "thingie"

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import User
 from .serializer import UserSerializer
 
@@ -7,12 +7,14 @@ from .serializer import UserSerializer
 class createUser(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
 
 create_user = createUser.as_view()
 
 class listUser(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 list_user = listUser.as_view()
 
@@ -20,6 +22,23 @@ class detailUser(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = "pk"
+    permission_classes = [permissions.IsAuthenticated]
 
 detail_user = detailUser.as_view()
 
+class deleteUser(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "pk"
+    permission_classes = [permissions.IsAdminUser]
+
+delete_user = deleteUser.as_view()
+
+
+class updateUser(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "pk"
+    permission_classes = [permissions.IsAuthenticated]
+
+update_user = updateUser.as_view()
