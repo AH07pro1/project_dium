5378fdfcb76e757a96d6d7146e185c75b844b575
h-twins <creative.h.twins@gmail.com>
2022-12-27 15:24:11 -0500

    Changed l_name and f_name length

--- backend/user/models.py
+++ backend/user/models.py
@@ -4,8 +4,8 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class User(models.Model):
-    f_name = models.CharField(max_length=10)
-    l_name = models.CharField(max_length=10)
+    f_name = models.CharField(max_length=15)
+    l_name = models.CharField(max_length=15)
     profile_pic = models.URLField(max_length=10000)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=25)
