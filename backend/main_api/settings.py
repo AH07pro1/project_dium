45151fa8929dead3b600a641cc01afd5b1382db1
AH07pro1 <adam.hilali@gmail.com>
2022-12-30 14:55:41 -0500

    Json fields

--- backend/main_api/settings.py
+++ backend/main_api/settings.py
@@ -146,10 +146,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000"
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+""" CORS_ORIGIN_ALLOW_ALL = False """
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:8000'
 )
 
-CSRF_COOKIE_NAME = "XSRF-TOKEN"
+CSRF_COOKIE_NAME = "csrftoken"
