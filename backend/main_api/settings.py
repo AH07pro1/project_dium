b5b4c5b1245eefcecc37a696211997bf74104a57
h-twins <creative.h.twins@gmail.com>
2023-04-30 09:49:50 -0400

    changes

--- backend/main_api/settings.py
+++ backend/main_api/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-a$hbb2#x5wcbge@qu^0gal22w(7)n4$)do&rdhu7ihb)suv@s5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh"] #"localhost",
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
